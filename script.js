700b786d4e1f5350f4983c513c43ed31ba2b5d96
Annepetrosyan <anpetrosyan1998@gmail.com>
2023-02-06 17:39:36 +0400

    1

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-const x = 10;
-if ((x = 10)) {
-  console.log("10");
-}
+// const x = 10;
+// if ((x = 10)) {
+//   console.log("10");
+// }
+// console.log("Anna");
+
+// console.log("Anna");
+// const calcAge = (birthYear) => 2023 - birthYear;
+// console.log(calcAge(1998));
+
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
