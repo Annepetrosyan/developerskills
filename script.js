90d89781d97ea82ae132597f4e782ff2453d0932
Annepetrosyan <anpetrosyan1998@gmail.com>
2023-02-06 18:37:06 +0400

    2

--- script.js
+++ script.js
@@ -14,3 +14,21 @@
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
 const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+//amplitute is difference between lowest and highest temp
+// compute max and min temp
+// ? sensor error and ? to do
+//--ignore sensore errors
+//-- find max value
+// -- find min value
+//-- subtracte min from max and return value
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  for (let i = 0; i < temps.lenght; i++) {
+    if (temps[i] > max) max = temps[i];
+  }
+  console.log(max);
+};
+
+calcTempAmplitude([3, 4, 8, 9, 5, 6, -1]);
